692cfc9fab1a9421f5670d05c0a1c0795ee42aff
Dorian <irinel.buraga@orange.com>
2020-12-12 17:20:45 +0200

    pentru cei ocupati

--- vconc.py
+++ vconc.py
@@ -1,5 +1,6 @@
 #dovadă că nu-s pleavă
-
+import os, cv2  #opencv-python==4.20.0.32
+from pprint import pprint
 def vconcat_resize(img_list, interpolation=cv2.INTER_CUBIC): 
       # take minimum width 
     w_max = max(img.shape[1] for img in img_list) 
@@ -13,4 +14,8 @@ def vconcat_resize(img_list, interpolation=cv2.INTER_CUBIC):
     # im_list_rewrite = [cv2.copyMakeBorder(img,0,0,0,0 if (img2.shape[1] == w_max for img in img_list) else w_max,cv2.BORDER_CONSTANT,value=white) for img in img_list]
     # im_list_resize = [cv2.resize(img, (w_max, int(img.shape[0] * w_max / img.shape[1])), interpolation = interpolation) for img in im_list_rewrite] 
     # return final image 
-    return cv2.vconcat(im_list_rewrite) 
+    return cv2.vconcat(im_list_rewrite)
+
+x = [file if file.endswith(".png") else 0 for file in os.listdir()]
+while 0 in x: x.remove(0)
+vconcat_resize(x)
